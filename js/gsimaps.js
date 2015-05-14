93cd4397d53c877bd0c4f0288681e26700d3032d
johofukyu <gsi-ina@ml.mlit.go.jp>
2015-05-14 10:31:04 +0900

    Update gsimaps.js , layers1.txt

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -684,7 +684,7 @@ CONFIG.BASETILES = [
 		url : 'http://{s}.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg',
 		subdomains : [ 'cyberjapandata-t1', 'cyberjapandata-t2', 'cyberjapandata-t3' ],
 		icon : 'image/map/djbmo.png',
-		"legendUrl" : "http://www.gsi.go.jp/gazochosa/gazochosa40001.html",
+		"legendUrl" : "http://maps.gsi.go.jp/development/ichiran.html#ort",
 		maxNativeZoom:17,
 		errorTileUrl : 'image/map/no-data-ort.png',
 		minZoom : 2
