a3b50f47ca87220a5da567173e82f5879e30563c
johofukyu <johofukyu@users.noreply.github.com>
2016-10-22 21:09:32 +0900

    Update gsimaps.js
    平成28年鳥取県中部地震のレイヤ追加

--- js/gsimaps.js
+++ js/gsimaps.js
@@ -23,6 +23,7 @@ CONFIG.layerBaseDefaultID = "std";
 CONFIG.layerBaseFolder    = "ベースマップ";
 CONFIG.layerBaseFolderSYS = "GSI.MAP.BASE";
 CONFIG.layers = [
+	'./layers_txt/layers_20161021tottori.txt',
 	'./layers_txt/layers_20160830typhoon10.txt',
 	'./layers_txt/layers_20160820typhoon11_9.txt',
 	'./layers_txt/layers_20160414kumamoto.txt',
@@ -35,7 +36,7 @@ CONFIG.layers = [
 ];
 
 //キャッシュ（Layers.txt）
-CONFIG.LOADLAYERSTXTCACHE = true;
+CONFIG.LOADLAYERSTXTCACHE = false;
 
 //キャッシュ（ココタイル）
 CONFIG.LOADCOCOTILECACHE = true;
